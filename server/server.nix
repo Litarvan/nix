fc02fa9c9a3ab3169e6060a7ac33e328e647e116
Adrien Navratil <adrien1975@live.fr>
2021-01-02 03:03:10 +0100

    Enabled nat in server configuration

--- server/server.nix
+++ server/server.nix
@@ -21,6 +21,12 @@
     usePredictableInterfaceNames = lib.mkForce false;
 
     nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+
+    nat = {
+      enable = true;
+      internalInterfaces = ["ve-+"];
+      externalInterface = "eth0";
+    };
   };
 
   services.openssh = {
