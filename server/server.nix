ee1b29d400d67d03b60868c268567630302948b0
Adrien Navratil <adrien1975@live.fr>
2021-01-02 02:09:40 +0100

    Fixed server.nix indentation

--- server/server.nix
+++ server/server.nix
@@ -16,10 +16,10 @@
   };
 
   networking = {
-      dhcpcd.enable = false;
-      usePredictableInterfaceNames = lib.mkForce false;
+    dhcpcd.enable = false;
+    usePredictableInterfaceNames = lib.mkForce false;
 
-      nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+    nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
   };
 
   services.openssh = {
