45c91a9e9fee3156b9943383201d4fec34953112
Adrien Navratil <adrien1975@live.fr>
2021-01-02 02:08:11 +0100

    Removed sydkilin IPv6 server route

--- server/sydkilin.nix
+++ server/sydkilin.nix
@@ -20,13 +20,10 @@
         routes = [ { address = "45.90.162.65"; prefixLength = 32; } ];
       };
 
-      ipv6 = {
-        addresses = [
-          { address = "2a0c:b642:4001:7:78bc:92ff:fe88:ccda"; prefixLength = 64; }
-          { address = "fe80::78bc:92ff:fe88:ccda"; prefixLength = 64; }
-        ];
-        routes = [ { address = "fe80::921b:eff:fe0b:186"; prefixLength = 32; } ];
-      };
+      ipv6.addresses = [
+        { address = "2a0c:b642:4001:7:78bc:92ff:fe88:ccda"; prefixLength = 64; }
+        { address = "fe80::78bc:92ff:fe88:ccda"; prefixLength = 64; }
+      ];
     };
   };
 
