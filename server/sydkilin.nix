00dd85d11fef635a7f7e868aed21d87fa8b0e284
Adrien Navratil <adrien1975@live.fr>
2021-01-02 02:35:52 +0100

    Cleaned up sydkilin network configuration

--- server/sydkilin.nix
+++ server/sydkilin.nix
@@ -19,18 +19,10 @@
     };
 
     defaultGateway = "45.90.162.65";
-    defaultGateway6 = "fe80::921b:eff:fe0b:186";
 
     interfaces.eth0 = {
-      ipv4 = {
-        addresses = [ { address = "45.90.162.86"; prefixLength = 27; } ];
-        routes = [ { address = "45.90.162.65"; prefixLength = 32; } ];
-      };
-
-      ipv6.addresses = [
-        { address = "2a0c:b642:4001:7:78bc:92ff:fe88:ccda"; prefixLength = 64; }
-        { address = "fe80::78bc:92ff:fe88:ccda"; prefixLength = 64; }
-      ];
+      ipv4.addresses = [ { address = "45.90.162.86"; prefixLength = 27; } ];
+      ipv6.addresses = [ { address = "2a0c:b642:4001:7:78bc:92ff:fe88:ccda"; prefixLength = 64; } ];
     };
   };
 
