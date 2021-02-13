ed822381f8f285f008468b347f4a573ff6d07973
Adrien Navratil <adrien1975@live.fr>
2021-02-13 18:15:13 +0900

    Better NGINX configuration

--- servers/nginx.nix
+++ servers/nginx.nix
@@ -6,7 +6,7 @@
     recommendedOptimisation = true;
     recommendedProxySettings = true;
 
-    sslCiphers = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DES-CBC3-SHA";
+    sslCiphers = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DES-CBC3-SHA";
 
     commonHttpConfig = ''
       ssl_session_timeout 1d;
@@ -30,7 +30,7 @@
           extraConfig = ''
               add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
               add_header X-Content-Type-Options "nosniff" always;
-              add_header X-Frame-Options "SAMEORIGIN" always;
+              add_header X-Frame-Options "DENY" always;
               add_header Referrer-Policy "no-referrer-when-downgrade" always;
 
               charset UTF-8;
