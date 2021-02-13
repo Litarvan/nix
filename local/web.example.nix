7df213b9ea5e71a1807be628b75aff45b68a5307
Adrien Navratil <adrien1975@live.fr>
2021-02-13 15:13:42 +0900

    Fixed example web configuration to use real paths

--- local/web.example.nix
+++ local/web.example.nix
@@ -3,8 +3,8 @@
 # Copy this file to web.nix
 
 {
-  # "sub1.mydomain.com" = folder /var/www;;
+  # "sub1.mydomain.com" = folder "/var/www";
   # "sub2.mydomain.com" = proxy http://localhost:1234;
-  # "sub3.mydomain.com" = folderWith /var/www2 "add_header Hello World;";
+  # "sub3.mydomain.com" = folderWith "/var/www2" "add_header Hello World;";
   # "sub4.mydomain.com" = proxyWith http://localhost:1235 "add_header Goodbye World;";
 }
